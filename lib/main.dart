314aeefc096a26dba63e7f8b2061536d5204f0de
Haddad Sarah <haddads686@gmail.com>
2022-05-10 01:42:27 +0100

    set up the geo locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,127 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:parking_locator/screens/search.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Parking App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Search(),
+        home: Home()
     );
   }
 }
+
+class Home extends  StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  bool servicestatus = false;
+  bool haspermission = false;
+  late LocationPermission permission;
+  late Position position;
+  double long = 0, lat = 0;
+  late StreamSubscription<Position> positionStream;
+
+  @override
+  void initState() {
+    checkGps();
+    super.initState();
+  }
+
+  checkGps() async {
+    //check if GPS is enabled:
+    servicestatus = await Geolocator.isLocationServiceEnabled();
+    if(servicestatus){
+      //check Location Permission or Request Location Permission:
+      permission = await Geolocator.checkPermission();
+
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          print('Location permissions are denied');
+        }else if(permission == LocationPermission.deniedForever){
+          print("'Location permissions are permanently denied");
+        }else{
+          haspermission = true;
+        }
+      }else{
+        haspermission = true;
+      }
+
+      if(haspermission){
+        setState(() {
+          //refresh the UI
+        });
+
+        getLocation();
+      }
+    }else{
+      print("GPS Service is not enabled, turn on GPS location");
+    }
+
+    setState(() {
+      //refresh the UI
+    });
+  }
+
+  getLocation() async {
+    //get the current GPS locations such as Longitude and Latitude
+    position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    long = position.longitude;
+    lat = position.latitude;
+
+    setState(() {
+      //refresh UI
+    });
+//How to Listen to GPS Location: Longitude and Latitude Change Stream:
+
+    LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high, //accuracy of the location data
+      distanceFilter: 100, //minimum distance (measured in meters) a
+      //device must move horizontally before an update event is generated;
+    );
+
+    StreamSubscription<Position> positionStream = Geolocator.getPositionStream(
+        locationSettings: locationSettings).listen((Position position) {
+      long = position.longitude;
+      lat = position.latitude;
+
+      setState(() {
+        //refresh UI on update
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Parking App"),
+            backgroundColor: Colors.redAccent
+        ),
+        body: Column(children:<Widget> [
+          Container(
+            height: MediaQuery.of(context).size.height/3,
+            width: MediaQuery.of(context).size.width,
+            child: GoogleMap(
+              initialCameraPosition:CameraPosition(
+                target: LatLng(long,lat),
+                zoom: 14.4746,
+
+              ),
+            ),
+          )
+        ],
+        )
+    );
+  }
+}
+
